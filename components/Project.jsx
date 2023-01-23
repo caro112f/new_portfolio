19a75e8442c920fd5e08af613a8c2c8d213f756c
Caroline Nielsen <caro112f@stud.kea.dk>
2023-01-23 19:23:50 +0100

    alt fix

--- components/Project.jsx
+++ components/Project.jsx
@@ -22,7 +22,7 @@ export default function Project({ slug, title, date, featuredImage, description,
     <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.5 }} viewport={{ once: true }}>
       <section className="text-black grid lg:grid-cols-2 py-6 my-10 md:my-20">
         <div className="px-6 py-2">
-          <Image src={img.src} alt={img.alt} width={img.width} height={img.height} priority sizes={img.sizes} objectFit="contain" />
+          <Image src={img.src} alt={title} width={img.width} height={img.height} priority sizes={img.sizes} objectFit="contain" />
         </div>
         <div className={`${gridchecker()} bg-gray-100 p-8 shadow-md md:flex ${oddchecker() === true ? "md:justify-start" : "md:justify-end"}`}>
           <div className={`max-w-[700px] m-auto`}>
